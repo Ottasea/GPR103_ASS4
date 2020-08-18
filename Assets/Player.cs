9d0fce874b2264c8d52fd833f5b35ebdf11d896a
DESKTOP-2MDF6JB\Ottasea <theottasea@gmail.com>
2020-08-18 17:45:38 +1000

    no message

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -111,6 +111,8 @@ public class Player : MonoBehaviour
                         transform.position = playerPos;
 
                     }
+                    
+                    break;
                 }
                 else
                 {
