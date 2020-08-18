34d36049b78f13656e4a15c7baef858df85a5cb9
DESKTOP-2MDF6JB\Ottasea <theottasea@gmail.com>
2020-08-18 11:36:23 +1000

    updated vehicle script to allow for a pause time and organized first row of vehicles into the scene

--- Assets/Vehicle.cs
+++ Assets/Vehicle.cs
@@ -11,6 +11,9 @@ public class Vehicle : MonoBehaviour
     public float speed; //This variable is to be used to control the speed of the vehicle.
     public Vector2 startingPosition; //This variable is to be used to indicate where on the map the vehicle starts (or spawns)
     public Vector2 endPosition; //This variablle is to be used to indicate the final destination of the vehicle.
+    public bool paused = true;
+    public float runTime;
+    float currentRunTime = 0.0f;
 
 
     // Start is called before the first frame update
@@ -21,7 +24,12 @@ public class Vehicle : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * speed * moveDirection);
+        currentRunTime += Time.deltaTime;
+        if (currentRunTime >= runTime)
+        {
+            paused = false;
+            transform.Translate(Vector2.right * Time.deltaTime * speed * moveDirection);
+        }
         
         if ((transform.position.x * moveDirection) > (endPosition.x * moveDirection))
         {
